063b9b700d0b7da919f33c9325bf0cb6514b46e8
kuntalashivasairahul <kuntalashivasairahul10d@gmail.com>
2026-03-26 15:56:27 +0530

    Connect frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Attach token from localStorage (MVP token from login/register response)
+// Attach token from localStorage
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("skillswap_token");
   if (token) {
